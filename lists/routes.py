172d3a9f2d5ba869531a7571d31877142966444e
Bryan W. Kao <bryan.w.kao@gmail.com>
2024-05-08 22:14:29 -0500

    add GET /lists/<listId>

--- lists/routes.py
+++ lists/routes.py
@@ -3,6 +3,7 @@ from db import db
 from .models import List
 from .serializers import ListSchema
 from flask import jsonify
+import uuid
 
 
 @bp.route('/', methods=['GET'])
@@ -11,4 +12,12 @@ def index():
     lists = db.session.execute(stmt).scalars().all()
     schema = ListSchema(many=True)
     result = schema.dumps(lists)
+    return result
+
+@bp.route('/<listId>', methods=['GET'])
+def getList(listId):
+    listUuid = uuid.UUID(listId)
+    list = db.session.query(List).filter(List.uuid == listUuid).first()
+    schema = ListSchema()
+    result = schema.dumps(list)
     return result
