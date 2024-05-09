c076bce994d69bde8d44897bd6457e664c156f28
Bryan W. Kao <bryan.w.kao@gmail.com>
2024-05-08 21:56:20 -0500

    add uuid field

--- lists/models.py
+++ lists/models.py
@@ -1,5 +1,6 @@
+import uuid
 from dataclasses import dataclass
-from sqlalchemy import String
+from sqlalchemy import String, Uuid
 from sqlalchemy.orm import Mapped, mapped_column
 from db import db
 from db.timestamp_mixin import TimestampMixin
@@ -10,3 +11,4 @@ class List(TimestampMixin, db.Model):
     __tablename__ = "lists"
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column(String(255))
+    uuid: Mapped[Uuid] = mapped_column(Uuid, unique=True, default=uuid.uuid4())
--- lists/serializers.py
+++ lists/serializers.py
@@ -2,7 +2,7 @@ from marshmallow import Schema, fields
 
 
 class ListSchema(Schema):
-    id = fields.Integer()
+    uuid = fields.UUID(data_key="listId")
     name = fields.Str()
     created_at = fields.DateTime(data_key="createdAt")
     updated_at = fields.DateTime(data_key="updatedAt")
