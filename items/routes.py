35e914f44ed8ddc14b6cdd5f2155bfd756c532f7
Bryan W. Kao <bryan.w.kao@gmail.com>
2024-05-14 22:08:40 -0500

    GET /items

--- items/routes.py
+++ items/routes.py
@@ -1,1 +1,13 @@
+from . import bp
+from db import db
 from .models import Item
+from .serializers import ItemSchema
+
+
+@bp.route('/', methods=['GET'])
+def index():
+    stmt = db.select(Item).order_by(Item.updated_at)
+    items = db.session.execute(stmt).scalars().all()
+    schema = ItemSchema(many=True)
+    result = schema.dumps(items)
+    return result
