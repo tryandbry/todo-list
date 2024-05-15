8ee7bb88b982d14c2bcbaa4f71a2af21663358e3
Bryan W. Kao <bryan.w.kao@gmail.com>
2024-05-14 22:15:29 -0500

    GET /items/<item_id>

--- items/routes.py
+++ items/routes.py
@@ -2,6 +2,10 @@ from . import bp
 from db import db
 from .models import Item
 from .serializers import ItemSchema
+from .validators import GetItemValidationSchema
+from flask import request, abort
+from marshmallow import ValidationError
+import uuid
 
 
 @bp.route('/', methods=['GET'])
@@ -10,4 +14,20 @@ def index():
     items = db.session.execute(stmt).scalars().all()
     schema = ItemSchema(many=True)
     result = schema.dumps(items)
+    return result
+
+@bp.route('/<item_id>', methods=['GET'])
+def show(item_id):
+    try:
+        path_params = GetItemValidationSchema().load(request.view_args)
+    except ValidationError as err:
+        return err.messages, 400
+
+    item_uuid = uuid.UUID(path_params['item_id'])
+    item = db.session.query(Item).filter(Item.uuid == item_uuid).first()
+    if item == None:
+        abort(404)
+
+    schema = ItemSchema()
+    result = schema.dumps(item)
     return result
--- items/validators.py
+++ items/validators.py
@@ -1,5 +1,11 @@
-from marshmallow import Schema, fields, EXCLUDE 
+from marshmallow import Schema, fields, validate, EXCLUDE
 
+
+class GetItemValidationSchema(Schema):
+    item_id = fields.Str(validate=validate.Regexp(
+        regex=r'^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$',
+        error="Invalid UUID"
+    ))
 
 class GetItemsValidationSchema(Schema):
     completed = fields.Bool()
