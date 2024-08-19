9d8b7b4035c8985fa123ce0d3cb7afc233989fb7
Bryan W. Kao <bryan.w.kao@gmail.com>
2024-08-19 09:07:08 -0500

    nest listId to item serializer

--- items/serializers.py
+++ items/serializers.py
@@ -1,9 +1,11 @@
 from marshmallow import Schema, fields
+from lists.serializers import ListSchema
 
 
 class ItemSchema(Schema):
     uuid = fields.UUID(data_key="itemId")
     name = fields.Str()
     completed = fields.Bool()
+    list = fields.Nested(ListSchema(only=("uuid",)))
     created_at = fields.DateTime(data_key="createdAt")
     updated_at = fields.DateTime(data_key="updatedAt")
